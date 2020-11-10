992bd3bf9173a808712c9e10fa4aadafdb929353
Gavin Phill <carpe.gavin@gmail.com>
2020-11-10 15:14:29 -0600

    added text input fields to the rows using HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 var dateDisplayEl = $('#currentDay');
 
 function displayDate() {
-    var rightNow = moment().format("MMM Do YY"); 
+    var rightNow = moment().format("MMM Do YYYY"); 
     dateDisplayEl.text(rightNow);
   }
   
+displayDate()
+
+// $('[placeholder]').focus(function() {
+//     var input = $(this);
+//     if (input.val() == input.attr('placeholder')) {
+//       input.val('');
+//       input.removeClass('placeholder');
+//     }
+//   }).blur(function() {
+//     var input = $(this);
+//     if (input.val() == '' || input.val() == input.attr('placeholder')) {
+//       input.addClass('placeholder');
+//       input.val(input.attr('placeholder'));
+//     }
+//   }).blur();
